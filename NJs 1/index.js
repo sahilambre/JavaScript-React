a33b554c4df23ebb150bfca13259f0c6cdca4378
Sahil Mukesh Ambre <59484827+sahilambre@users.noreply.github.com>
2023-10-24 20:31:28 -0400

    add gitingore

---
+++ NJs 1/index.js
@@ -0,0 +1,18 @@
+// getName();
+// console.log(getName); //prints the whole function
+// console.log(x); //undefined
+var x = 7;
+
+// var getName = () => {
+//   console.log("sahilsahil");
+// };
+
+function getName() {
+  //   console.log(x);
+
+  console.log("Sahil");
+}
+
+getName();
+console.log(x);
+console.log(getName);
